b32f589ef2d7ce7f6f52c16030bd6d90b1a15e07
Claudio Matos <claudiomatos@gmail.com>
2024-10-23 21:06:32 -0300

    (23/10) Ajusta geolocation by Xandy

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,30 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
-export default class Main extends Component {
-  render() {
-    return (
-      <Container>
-        <Text>Teste</Text>
-      </Container>
-    );
-  }
+import React, {useState} from 'react';
+import MapView from 'react-native-maps';
+import useLocation from '../hooks/useLocation';
+
+export default function Main({navigation}) {
+  const [latitude, setLatitude] = useState(-20.5149138);
+  const [longitude, setLongitude] = useState(-47.4006943);
+
+  const {coords, errorMsg} = useLocation();
+
+  return (
+    <MapView
+      showsUserLocation={true}
+      showsMyLocationButton={false}
+      toolbarEnabled={false}
+      style={{
+        height: '100%',
+        width: '100%',
+        position: 'absolute',
+      }}
+      initialRegion={{
+        latitude,
+        longitude,
+        latitudeDelta: 0.010,
+        longitudeDelta: 0.010,
+        ...coords,
+      }}
+    />
+  );
 }
